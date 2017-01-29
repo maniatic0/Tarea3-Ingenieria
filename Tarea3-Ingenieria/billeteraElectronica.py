32e233d42465c3dc6a599e04d437a1d549215fc4
Christian Oliveros <01christianol01@gmail.com>
2017-01-28 23:28:56 -0400

    Implementada Funcionalidad Basica de Clase

--- Tarea3-Ingenieria/billeteraElectronica.py
+++ Tarea3-Ingenieria/billeteraElectronica.py
@@ -6,6 +6,7 @@ Created on Jan 28, 2017
 @author: Daniel Varela
 '''
 from datetime import datetime
+from builtins import str, int
 
 class BilleteraElectronica(object):
     '''
@@ -27,23 +28,48 @@ class BilleteraElectronica(object):
     _registoDebitos = []
 
 
-    def __init__(self, identificador, nombres, apellidos, ci, pin):
+    def __init__(self, identificador, nombres: str, apellidos: str, ci: int, pin: int):
         '''
-        Constructor
+        Constructor de la Clase BilleteraElectronica
+        @param identificador: Identificador de la Persona
+        @param nombres: Nombres de la Persona
+        @param apellidos: Apellidos de la Persona
+        @param ci: Cedula de la Persona
+        @param pin: PIN de la Billetera de la Persona
         '''
-        pass
+        if not isinstance(nombres, str) or nombres == "":
+            raise Exception("Nombres tiene que ser un String no Vacio")
+        
+        if not isinstance(apellidos, str) or apellidos == "":
+            raise Exception("Apellidos tiene que ser un String no Vacio")
+        
+        if not isinstance(ci, int) or ci <= 0:
+            raise Exception("Cedula tiene que ser un Entero mayor que Cero")
+        
+        if not isinstance(pin, int) or pin < 0:
+            raise Exception("PIN tiene que ser un Entero mayor o igual que Cero")
+        
+        self._identificador = identificador
+        self._nombres = nombres
+        self._apellidos = apellidos
+        self._ci = ci
+        self._pin = pin
     
     def getIdentificador(self):
-        pass
+        '''@return: Identificador de la Persona'''
+        return self._identificador
     
     def getNombres(self):
-        pass
+        '''@return: Nombres de la Persona'''
+        return self._nombres
     
     def getApellidos(self):
-        pass
+        '''@return: Apellidos de la Persona'''
+        return self._apellidos
     
     def getCI(self):
-        pass
+        '''@return: CI de la Persona'''
+        return self._ci
     
     def saldo(self):
         pass
--- Tarea3-Ingenieria/testBilleteraElectronica.py
+++ Tarea3-Ingenieria/testBilleteraElectronica.py
@@ -9,16 +9,34 @@ import unittest
 from billeteraElectronica import BilleteraElectronica
 
 class Test(unittest.TestCase):
-
-
+    
+    def helperConstructorTest(self, identificador, nombres, apellidos, ci, pin):
+        '''
+        Funcion de apoyo para probar construcion de clase correcta
+        @param identificador: Identificador de la Persona
+        @param nombres: Nombres de la Persona
+        @param apellidos: Apellidos de la Persona
+        @param ci: Cedula de la Persona
+        @param pin: PIN de la Billetera de la Persona
+        '''
+        billetera = BilleteraElectronica(identificador, nombres, apellidos, ci, pin)
+        self.assertEqual(billetera.getIdentificador(), identificador, "Identificador Distinto")
+        self.assertEqual(billetera.getNombres(), nombres, "Nombres Distintos")
+        self.assertEqual(billetera.getApellidos(), apellidos, "Apellidos Distintos")
+        self.assertEqual(billetera.getCI(), ci, "CI Distintas")
+        self.assertEqual(billetera._pin, pin, "PIN Distinto")
+        
     def testConstruccion(self):
-        billetera = BilleteraElectronica(1, "Juan", "Gomez", 24666888, 1234)
-        self.assertEqual(billetera.getIdentificador(), 1, "Identificador Distinto")
-        self.assertEqual(billetera.getNombres(), "Juan", "Nombres Distintos")
-        self.assertEqual(billetera.getApellidos(), "Gomez", "Apellidos Distintos")
-        self.assertEqual(billetera.getCI()(), 24666888, "CI Distintas")
-        self.assertEqual(billetera._pin, 1234, "PIN Distinto")
+        '''Caso Interior'''
+        self.helperConstructorTest(1, "Juan", "Gomez", 24666888, 1234)
 
+    def testConstruccionConUnicode(self):
+        '''Caso Interior'''
+        self.helperConstructorTest(1, "Adèle", "Gómez", 300, 1324)
+        
+    def testConstruccionConVariosNombresYApellidos(self):
+        '''Caso Interior'''
+        self.helperConstructorTest(1, "Adèle Alexia", "Gómez Ñañe", 700000000, 3000)
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
