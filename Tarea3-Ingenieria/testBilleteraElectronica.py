2324e7704e8db145bfbc3222db41a11f01aac8eb
daniel <daniel@daniel-desktop>
2017-01-29 13:40:30 -0400

    Implementacion inicial casos prueba saldo y registro
    Se han implementado los casos de prueba
    
    testBalanceSaldo
    testRecargaSaldo
    testConsumirSaldo
    testRegistroSaldo
    testRegistroDebito
    
    Los cuales deberian todos fallar ya que no se han implementado aun los
    metodos que utilizan.

--- Tarea3-Ingenieria/testBilleteraElectronica.py
+++ Tarea3-Ingenieria/testBilleteraElectronica.py
@@ -6,6 +6,7 @@ Created on Jan 28, 2017
 @author: Daniel Varela
 '''
 import unittest
+from datetime import date
 from billeteraElectronica import BilleteraElectronica
 
 class Test(unittest.TestCase):
@@ -37,7 +38,44 @@ class Test(unittest.TestCase):
     def testConstruccionConVariosNombresYApellidos(self):
         '''Caso Interior'''
         self.helperConstructorTest(1, "Adèle Alexia", "Gómez Ñañe", 700000000, 3000)
+        
+    def testBalanceSaldo(self):
+        billetera = BilleteraElectronica(1,"Alexander","Infante",13102741,4200)
+        self.assertEqual(billetera.saldo(),0,"Balance Saldo Distinto")
+        
+    def testRecargaSaldo(self):
+        '''Caso Interior'''
+        billetera = BilleteraElectronica(1,"John","Palma",26007718,1337)
+        billetera.recargar(100,date.today(),909091)
+        self.assertEqual(billetera.saldo(),100,"Recarga Saldo Distinto")
+        
+    def testConsumirSaldo(self):
+        '''Caso Interior'''
+        billetera = BilleteraElectronica(1,"Cesar","Jose",10231940,5901)
+        billetera.consumir(-15,date.today(),843213,5901)
+        self.assertEqual(billetera.saldo(),-15,"Consumo Saldo Distinto")
+        
+    def testRegistroCredito(self):
+        '''Caso Interior'''
+        billetera = BilleteraElectronica(1,"Ricardo","Gomez",26697166,6412)
+        billetera.recargar(200,date.today(),666999)
+        tamano = len(billetera._registroCreditos)-1
+        self.assertEqual(billetera._registroCreditos[tamano][0],200,"Registro Saldo Distinto")
+        self.assertEqual(billetera._registroCreditos[tamano][1],date.today(),"Registro Fecha Distinto")
+        self.assertEqual(billetera._registroCreditos[tamano][2],666999,"Registro IDlocal Distinto")
+        
+    def testRegistroDebito(self):
+        '''Caso Interior'''
+        billetera = BilleteraElectronica(1,"Oscar","Silva",9039084,10120)
+        billetera.consumir(-300,date.today(),131141,10120)
+        tamano = len(billetera._registroCreditos)-1
+        self.assertEqual(billetera._registroCreditos[tamano][0],-300,"Registro Saldo Distinto")
+        self.assertEqual(billetera._registroCreditos[tamano][1],date.today(),"Registro Fecha Distinto")
+        self.assertEqual(billetera._registroCreditos[tamano][2],date.today(),"Registro IDlocal Distinto")
+        
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
+    
+    
